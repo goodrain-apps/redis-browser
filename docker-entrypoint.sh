d9891ee3dc9137209b555f5116b35a4e40f544a5
zhouyq <zyq916@gmail.com>
2016-06-28 13:36:04 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,4 +6,4 @@ cd /opt/redis-browser
 
 sleep ${PAUSE:-0}
 
-./bin/redis-browser -B $POD_NET_IP -U redis://${REDIS_HOST}:${REDIS_PORT}/0
+./bin/redis-browser -B 0.0.0.0 -U redis://${REDIS_HOST}:${REDIS_PORT}/0
