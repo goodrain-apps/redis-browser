cb56fdb49e54fcc326e7cbbe1f7db820d9da4907
zhouyq <zyq916@gmail.com>
2016-07-01 14:55:59 +0800

    add ENABLE_AUTH env

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,19 @@
 
 cd /opt/redis-browser
 
+
+if [ $ENABLE_AUTH != "" ];then
+
+cat >> /opt/redis-browser/lib/redis-browser.rb << END
+RedisBrowser::Web.class_eval do
+  use Rack::Auth::Basic, "Protected Area" do |username, password|
+    username == 'foo' && password == 'bar'
+  end
+end
+END
+
+fi
+
 sleep ${PAUSE:-0}
 
 ./bin/redis-browser -B 0.0.0.0 -U redis://${REDIS_HOST}:${REDIS_PORT}/0
